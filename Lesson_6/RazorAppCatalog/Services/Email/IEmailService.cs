bfb134b7caebe9fb9a3fd3eef142e2ff707b389d
VolAnton <_Tocuk@mail.ru>
2022-07-07 19:31:05 +0300

    Домашнее задание №6

---
+++ Lesson_6/RazorAppCatalog/Services/Email/IEmailService.cs
@@ -0,0 +1,8 @@
+﻿namespace RazorAppCatalog.Services.Email
+{
+    public interface IEmailService
+    {
+        public Task SendEmailAsync(Product product);
+
+    }
+}
---
+++ Lesson_6/RazorAppCatalog/ViewModels/Catalog.cs
@@ -0,0 +1,28 @@
+﻿using RazorAppCatalog.Services.Email;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace RazorAppCatalog.ViewModels
+{
+    public sealed class Catalog : ICatalog
+    {
+        private readonly ConcurrentDictionary<int, Product> _productDict = new();
+
+        public int Count => _productDict.Count;
+
+        public async void Add(Product product, IEmailService emailService)
+        {
+            if (_productDict.TryAdd(product.Id, product))
+            {
+                await emailService.SendEmailAsync(product);
+            }
+        }
+
+        public void Remove(Product product) => _productDict.TryRemove(product.Id, out _);
+
+        public void Clear() => _productDict.Clear();
+
+        public IReadOnlyCollection<Product> GetAll() => _productDict.Values.ToArray();
+
+    }
+}
