bd168696d033eea086c327f19b16e7f348e919a9
VolAnton <_Tocuk@mail.ru>
2022-07-11 19:14:17 +0300

    Домашнее задание №7

---
+++ Lesson_7/RazorAppCatalog/ViewModels/ICatalog.cs
@@ -0,0 +1,18 @@
+﻿using RazorAppCatalog.Services.Email;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace RazorAppCatalog.ViewModels
+{
+    public interface ICatalog
+    {
+        public void Add(Product product, IEmailService emailService, CancellationToken cancellationToken);
+
+        public void Remove(Product product);
+
+        public void Clear();
+
+        public IReadOnlyCollection<Product> GetAll();
+
+    }
+}
